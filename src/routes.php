c37a66159300997619c6611a7d1edb255b013184
betech-TaiLM <65880494+betech-TaiLM@users.noreply.github.com>
2022-05-19 16:28:19 +0700

    update

--- src/routes.php
+++ src/routes.php
@@ -1,17 +1,1 @@
 <?php
-
-Route::group(['namespace' => "Bisync\IntroStep\Controllers"], function () {
-
-    Route::group(['namespace' => 'Api', 'prefix' => 'intro-step-admin/api', 'as' => 'intro-step-admin.api.', 'middleware' => config('intro-step.api_middleware')], function () {
-        Route::resource('steps', 'StepsController');
-        Route::resource('user', 'UserController')->only('store');
-    });
-
-    Route::group(['prefix' => 'intro-step-admin', 'as' => 'intro-step-admin.', 'middleware' => config('intro-step.web_middleware')], function () {
-        Route::get('/', 'IntroStepController@index');
-
-        Route::resource('api', "Api\IndexController")->middleware(config('intro-step.api_middleware'));
-    });
-
-    Route::resource('intro-step-admin', 'IntroStepController')->middleware(config('intro-step.web_middleware'));
-});
